9980e2a2dbbfb4ffd3bc12062c2cf14116cb6e05
Hao Yuan <haomichaelyuan@gmail.com>
2022-12-15 19:25:10 +1100

    commit tree 9b312bd776ef2e2eb84a6204a7def5971974a559

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
 // Your code here
-document.addEventListener("keydown", function (e) {
+// document.addEventListener("keydown", function (e) {
+//     if (e.key === "ArrowLeft") {
+//       moveDodgerLeft();
+//     }else if(e.key === "ArrowRight"){
+//         moveDodgerRight();
+//     }
+//   });
+
+
+//   function moveDodgerLeft() {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
+
+//     if (left > 0) {
+//       dodger.style.left = `${left - 1}px`;
+//     }
+//   }
+
+// function moveDodgerRight(){
+//     const leftNumbers = dodger.style.left.replace("px","");
+//     const left = parseInt(leftNumbers,10);
+//     if(left<360){
+//         dodger.style.left=`${left+1}px`
+//     }
+// }
+
+
+
+const dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = 'pink'
+document.addEventListener('keydown', move)
+function move(e) {
     if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }else if(e.key === "ArrowRight"){
-        moveDodgerRight();
+        moveDodgerLeft()
+
     }
-  });
 
+    else if (e.key === "ArrowRight") {
 
-  function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
-  }
-
-function moveDodgerRight(){
-    const leftNumbers = dodger.style.left.replace("px","");
-    const left = parseInt(leftNumbers,10);
-    if(left<360){
-        dodger.style.left=`${left+1}px`
     }
 }
-
-
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+    if (left < 360)
+        dodger.style.left = `${left + 1}px`
+}
